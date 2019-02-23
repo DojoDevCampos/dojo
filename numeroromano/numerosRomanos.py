68d60fb3ac7ddc9d341e172c4693992c6943cec3
DojoDevCampos <dojodevcampos@gmail.com>
2019-02-23 18:30:24 +0000

    parte 2 do dojo da biblioteca

--- numeroromano/numerosRomanos.py
+++ numeroromano/numerosRomanos.py
@@ -1,6 +1,6 @@
 class NumerosRomanos:
     valoresRomanoDecimal = { 
-            'I': 1, 
+            'I': 1,
             'V': 5,
             'X': 10,
             'L': 50,
@@ -19,13 +19,51 @@ class NumerosRomanos:
             }
     def __init__ (self):
         pass
-    def decimal_romano(self,numero_decimal):      
+
+
+
+
+
+    def decimal_romano(self, numero_decimal):
         resultado_romano = ''
         decimal_temporario = numero_decimal
+        invertida = sorted(self.valoresDecimalRomano.keys(), reverse=True)
+
+        def dr_letra_unica(decimal, resultado_romano, decimal_temporario):
+
+            resultado_romano += self.valoresDecimalRomano[decimal]
+            decimal_temporario -= decimal
+            return resultado_romano, decimal_temporario
+
+        def dr_letra_composta(decimal, resultado_romano, decimal_temporario):
+            valor_decrementado = decrescimo(decimal_temporario)
+            romano_decrementado = self.valoresDecimalRomano[valor_decrementado]
+            resultado_romano += "{}{}".format(romano_decrementado, self.valoresDecimalRomano[decimal])
+            decimal_temporario -= decimal - valor_decrementado
+            return resultado_romano, decimal_temporario
+
+        def decrescimo(decimal_temporario):
+            if decimal_temporario >= 400:
+                return 100
+            elif decimal_temporario >= 40:
+                return 10
+            elif decimal_temporario >= 4:
+                return 1
+            else:
+                return 0
+
+
         while decimal_temporario>0:
-            for x in sorted(self.valoresDecimalRomano.keys(),reverse=True): 
+            for x in invertida:
+                if (decimal_temporario <= 0):
+                    break
                 if (decimal_temporario >= x):
-                      resultado_romano += self.valoresDecimalRomano[x]
-                      decimal_temporario -= x
+                    resultado_romano, decimal_temporario = dr_letra_unica(x, resultado_romano, decimal_temporario)
+
+                elif (decimal_temporario >= (x-decrescimo(decimal_temporario))):
+                    resultado_romano, decimal_temporario = dr_letra_composta(x, resultado_romano, decimal_temporario)
+
+
+
+
         return resultado_romano
-        return self.valoresDecimalRomano[numero_decimal]
--- numeroromano/teste.py
+++ numeroromano/teste.py
@@ -4,20 +4,37 @@ from numerosRomanos import NumerosRomanos
 class testNumeroRomano(unittest.TestCase):
     def setUp(self):
         self.numero_romano = NumerosRomanos()
-    
-    def testdecimal_romano(self):
-        self.assertEqual(self.numero_romano.decimal_romano(1),
-                          'I', 'I falhou')
-        
-        self.assertEqual(self.numero_romano.decimal_romano(3), 
-                         'III', 'III falhou')
-        
-        self.assertEqual(self.numero_romano.decimal_romano(5),
-                          'V', 'V falhou')
 
-        self.assertEqual(self.numero_romano.decimal_romano(50),
-                          'L', 'L falhou')
-        
-        self.assertEqual(self.numero_romano.decimal_romano(2),
-                          'II', 'II falhou')
-        
+    def testdecimal_I(self):
+        self.assertEqual('I', self.numero_romano.decimal_romano(1),
+                         'I falhou')
+        self.assertEqual('III', self.numero_romano.decimal_romano(3),
+                         'III falhou')
+        self.assertEqual('II', self.numero_romano.decimal_romano(2),
+                         'II falhou')
+    def testdecimal_IV(self):
+        self.assertEqual('IV', self.numero_romano.decimal_romano(4),
+                         'IV falhou')
+    def testdecimal_V(self):
+        self.assertEqual('V', self.numero_romano.decimal_romano(5),
+                         'V falhou')
+    def testdecimal_IX(self):
+        self.assertEqual('IX', self.numero_romano.decimal_romano(9),
+                         'IX falhou')
+    def testdecimal_L(self):
+        self.assertEqual('L', self.numero_romano.decimal_romano(50),
+                          'L falhou')
+
+    def testdecimal_XIX(self):
+        self.assertEqual('XIX', self.numero_romano.decimal_romano(19),
+                         'XIX falhou')
+
+    def testdecimal_XL(self):
+        self.assertEqual('XL', self.numero_romano.decimal_romano(40),
+                         'XL falhou')
+
+
+
+#  para quem roda o python pelo terminal inclua a linha 39  e 40 e tenha o unittest instalado
+# if __name__ == '__main__':
+#     unittest.main()
